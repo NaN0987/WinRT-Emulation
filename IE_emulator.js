8e293923d3a788edc51d3d34b66d2e8d747aab28
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2026-05-21 22:29:36 -0600

    Initial commit 2

---
+++ IE_emulator.js
@@ -0,0 +1,267 @@
+// This will eventually be put in Windows_emulation.js once I emulate all of the 
+// necessary functions to allow base.js and ui.js to run in WinRT mode.
+// - Noah
+
+// I believe this is some sort of log for what the app is currently doing
+window.msWriteProfilerMark = window.msWriteProfilerMark || function (str) {
+    console.log("msProfilerMark: ", str);
+}
+
+// Function to run something immediately after the current execution stack
+window.setImmediate = window.setImmediate || function (func) {
+    return setTimeout(func, 0);
+};
+
+// Old IE object to manage gestures from touch screens
+class MSGesture {
+    // TODO: figure out how to actually implement this
+    constructor() {
+        this.target = null;
+    }
+
+    addPointer() {}
+}
+
+// Code to add "attachEvent" and "detachEvent" back as functions you can call on
+// elements, the document, and the window.
+// Currently, this replaces them with addEventListener and removeEventListener
+(function () {
+
+    // Function for converting old event names into their modern equivalents
+    const conversionMap = {
+         // IE10 / Windows 8 prefixed Pointer Events
+        MSPointerDown: "pointerdown",
+        MSPointerUp: "pointerup",
+        MSPointerMove: "pointermove",
+        MSPointerOver: "pointerover",
+        MSPointerOut: "pointerout",
+        MSPointerEnter: "pointerenter",
+        MSPointerLeave: "pointerleave",
+        MSPointerCancel: "pointercancel",
+
+        // Manipulation events (old IE touch model)
+        MSManipulationStateChanged: "pointermove",
+
+        // Old mouse wheel
+        mousewheel: "wheel",
+        DOMMouseScroll: "wheel",
+
+        // Legacy fullscreen
+        MSFullscreenChange: "fullscreenchange",
+        MSFullscreenError: "fullscreenerror",
+
+        // IE animation events
+        MSAnimationStart: "animationstart",
+        MSAnimationEnd: "animationend",
+        MSAnimationIteration: "animationiteration",
+
+        // IE transition events
+        MSTransitionEnd: "transitionend",
+
+        // Old mutation events (obsolete)
+        // DOMNodeInserted: "MutationObserver",
+        // DOMNodeRemoved: "MutationObserver",
+        // DOMSubtreeModified: "MutationObserver",
+
+        // Touch fallback mappings
+        touchstart: "pointerdown",
+        touchmove: "pointermove",
+        touchend: "pointerup",
+        touchcancel: "pointercancel",
+
+        // Mouse fallback mappings
+        mousedown: "pointerdown",
+        mousemove: "pointermove",
+        mouseup: "pointerup",
+        mouseenter: "pointerenter",
+        mouseleave: "pointerleave",
+        mouseover: "pointerover",
+        mouseout: "pointerout"
+    }
+    function modernizeEventName(event) {
+        // Do a manual conversion if it's available
+        if (conversionMap[event]) {
+            return conversionMap[event];
+        }
+
+        // Removes "on" from the beginning of event names
+        // Solves 50% of issues with old event names
+        return event.replace(/^on/, "");
+    }
+
+
+    // Override addEventListener to convert names
+    const originalAddEventListener = EventTarget.prototype.addEventListener;
+
+    EventTarget.prototype.addEventListener = function (type, listener, options) {
+        return originalAddEventListener.call(
+            this,
+            modernizeEventName(type),
+            listener,
+            options
+        );
+    };
+
+    // Override removeEventListener to convert names
+    const originalRemoveEventListener = EventTarget.prototype.removeEventListener;
+    EventTarget.prototype.removeEventListener = function (type, listener, options) {
+        return originalRemoveEventListener.call(
+            this,
+            modernizeEventName(type),
+            listener,
+            options
+        );
+    };
+
+    // Stores wrapped handlers per object
+    const eventMap = new WeakMap();
+    
+    // TODO: double-check that chat did this right
+    function attachEvent(eventName, handler) {
+
+        const type = modernizeEventName(eventName); // NOTE: this may not be necessary
+
+        // IE compatibility wrapper
+        const wrappedHandler = function (e) {
+
+            window.event = e;
+
+            // Note: "window" is technically right, but "this" is more modern
+            return handler.call(window, e);
+        };
+
+        // Create storage for this object
+        if (!eventMap.has(this)) {
+            eventMap.set(this, {});
+        }
+
+        const events = eventMap.get(this);
+
+        // Store wrapper by original handler
+        if (!events[type]) {
+            events[type] = new Map();
+        }
+
+        events[type].set(handler, wrappedHandler);
+
+        this.addEventListener(type, wrappedHandler);
+
+        return true;
+    }
+
+    // TODO: double-check that chat did this right
+    function detachEvent(eventName, handler) {
+
+        const type = modernizeEventName(eventName); // NOTE: this may not be necessary
+
+        const events = eventMap.get(this);
+
+        if (!events || !events[type]) {
+            return false;
+        }
+
+        const wrappedHandler = events[type].get(handler);
+
+        if (!wrappedHandler) {
+            return false;
+        }
+
+        this.removeEventListener(type, wrappedHandler);
+
+        events[type].delete(handler);
+
+        return true;
+    }
+
+    // Install polyfills
+    if (!Element.prototype.attachEvent) {
+        Element.prototype.attachEvent = attachEvent;
+    }
+
+    if (!Element.prototype.detachEvent) {
+        Element.prototype.detachEvent = detachEvent;
+    }
+
+    if (!Document.prototype.attachEvent) {
+        Document.prototype.attachEvent = attachEvent;
+    }
+
+    if (!Document.prototype.detachEvent) {
+        Document.prototype.detachEvent = detachEvent;
+    }
+
+    if (!Window.prototype.attachEvent) {
+        Window.prototype.attachEvent = attachEvent;
+    }
+
+    if (!Window.prototype.detachEvent) {
+        Window.prototype.detachEvent = detachEvent;
+    }
+
+    const ruleConversionMap = {
+        "-ms-user-select": "user-select",
+        "-ms-flexbox": "flex",
+        "-ms-flex-direction": "flex-direction"
+    }
+
+    // Function for converting style properties
+    function convertCssRules(styleString) {
+        for (const [from, to] of Object.entries(ruleConversionMap)) {
+            styleString = styleString.replaceAll(from, to);
+        }
+        return styleString;
+    }
+
+    // CSS emulation
+    document.addEventListener("DOMContentLoaded", async function() {
+
+        // Get every element
+        document.querySelectorAll('*').forEach(el => {
+            const style = getComputedStyle(el);
+            
+            // Check 1: ensure that "pointer-events: none" only applies to 
+            // the parent element and none of its children
+            if (style.pointerEvents === 'none') {
+                el.querySelectorAll('*').forEach(child => {
+                    if (!child.style.pointerEvents) {
+                        child.style.pointerEvents = 'auto';
+                    }
+                });
+            }
+
+            // Check 2: convert inline style rules to modern rules
+            if (el.style.textContent) {
+                const newStyle = convertCssRules(el.style.textContent);
+                el.style.textContent = newStyle;
+            }
+        });
+
+        // Iterate over all stylesheets
+        const links = document.querySelectorAll('link');
+        for (const link of links) {
+
+            if (link.rel !== "stylesheet") {
+                continue;
+            }
+            console.log("Sheet: " + link.href);
+            const response = await fetch(link.href);
+            
+            // Get text
+            let css = await response.text();
+
+            // Convert text
+            const newText = convertCssRules(css);
+
+            // Put new css rules in style element
+            if (css !== newText) {
+                const style = document.createElement("style");
+                style.textContent = newText;
+
+                document.head.appendChild(style);
+                
+                // Disable the old rules
+                link.disabled = true;
+            }
+        }
+    });
+})();
---
+++ Windows_emulator.js
@@ -0,0 +1,460 @@
+﻿
+// Fake windows object
+window.Windows = {
+
+    Foundation: {
+        // TODO: fill this class out more
+        Uri: class {
+            constructor(uri) {
+                this.rawUri = uri;
+
+                const u = new URL(uri);
+
+                this.absoluteUri = u.href;
+                this.schemeName = u.protocol.replace(":", "");
+                this.host = u.hostname;
+                this.path = u.pathname;
+                this.query = u.search;
+                this.fragment = u.hash;
+            }
+
+            toString() {
+                return this.absoluteUri;
+            }
+        }
+    },
+
+    ApplicationModel: {
+        // Some basic shit that every single program has
+        Activation: {
+            ActivationKind: {
+                // TODO: somehow add support for suspension and other types of activations
+                launch: "Windows.Launch"
+            },
+
+            // NOTE: by default, base.js will use "NotRunning"
+            ApplicationExecutionState: {
+                NotRunning: 0,
+                Running: 1,
+                Suspended: 2,
+                Terminated: 3,
+                ClosedByUser: 4
+            }
+        },
+
+        // Manages what happens when the user clicks "share"
+        DataTransfer: {
+            DataTransferManager: {
+                getForCurrentView() {
+                    return {
+                        addEventListener(string, func) {
+                            // TODO: find some way to emulate this
+                            // For now, sharing will do nothing
+                        }
+                    }
+                },
+
+                showShareUI() {
+                    // TODO: find some way to emulate this
+                    // For now, sharing will do nothing
+                }
+            }
+        },
+
+        // There's a special "design mode" for when you would test the 
+        // apps in Visual Studio
+        DesignMode: {
+            designModeEnabled: false
+        },
+
+        // Stuff for managing app id, license info, etc.
+        Store: {
+            CurrentApp: {
+                appId: null, // set later
+
+                licenseInformation: {
+                    isActive: true,
+
+                    productLicenses: {
+                        // TODO: implement this better
+                        lookup(name) {
+                            return {
+                                isActive: true
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+    },
+
+    // Functions for storing data about the game in files
+    Storage: {
+        ApplicationData: {
+            current: {
+
+                // TODO: make file emulation use actual files, and create a file class to represent files
+                localFolder: {
+
+                    // Function for accessing local files
+                    // Emulating this using localStorage
+                    getFileAsync(filename) {
+                        let localStorageData = localStorage.getItem("WinRT-localFolder:" + filename);
+
+                        return {
+                            done(success_func, error_func) {
+                                if (localStorageData === null) {
+                                    error_func("The file " + filename + " doesn't exists.")
+                                }
+                                else {
+                                    success_func({
+                                        name: filename,
+                                        data: localStorageData
+                                    })
+                                }
+                            }
+                        }
+                    },
+
+                    // Function for accessing local files
+                    // Emulating this using localStorage
+                    createFileAsync(filename, collisionOption) {
+                        let localStorageData = localStorage.getItem("WinRT-localFolder:" + filename);
+                        
+                        // Handle Collisions
+                        if (localStorageData != null) {
+                            switch (collisionOption) {
+                                // Call the error function
+                                case "FailIfExists": {    
+                                    return {
+                                        done(success_func, error_func) {
+                                            error_func("File " + filename + " already exists.");
+                                        }
+                                    }
+                                    break;
+                                }
+
+                                // Create a new file under a different name
+                                case "GenerateUniqueName": {
+                                    // TODO: make this better
+                                    filename = filename + " (1)";
+                                    localStorageData = null;
+                                    break;
+                                }
+
+                                // Pretend the file doesn't exist and overwrite it
+                                case "replaceExisting": {
+                                    localStorageData = null;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // If the file doesn't exist, create a new one
+                        if (localStorageData == null) {
+                            localStorage.setItem("WinRT-localFolder:" + filename, "");
+                            localStorageData = "";
+                        }
+
+                        // Construct file object
+                        let file = {
+                            name: filename,
+                            data: localStorageData,
+
+                            // TODO: make file streams actually work
+                            openAsync(fileAccessMode) {
+                                // Create stream object
+                                let stream = {
+                                    stream_data: "",
+
+                                    close() {},
+
+                                    flushAsync() {
+                                        data += this.stream_data;
+                                        localStorage.setItem("WinRT-localFolder:" + name, data);
+                                        return {
+                                            then(func) {
+                                                func();
+                                            }
+                                        }
+                                    }
+                                }
+
+                                return {
+                                    then(func) {
+                                        func(stream);
+                                    }
+                                }
+                            }
+                        }
+
+                        return {
+                            done(success_func, error_func) {
+                                success_func(file);
+                            },
+
+                            then(func) {
+                                func(file);
+                            }
+                        }
+                    },
+                },
+
+                roamingSettings: {
+                    values: new Proxy({}, {
+                        // Proxy wrapper to simulate a dictionary for roaming data to be stored in
+                        // Currently being emulated with localStorage
+                        get(target, prop) {
+                            if (typeof prop !== "string") return undefined;
+
+                            const item = localStorage.getItem("WinRT-roamingSettings:" + prop);
+                            
+                            if (item === null) {
+                                return undefined;
+                            }
+                            return item;
+                        },
+
+                        set(target, prop, value) {
+                            if (typeof prop !== "string") return false;
+                            localStorage.setItem("WinRT-roamingSettings:" + prop, value);
+                            return true; // required for Proxy sets
+                        }
+                    })
+                }
+            }
+        },
+
+        // Functions for reading/writing to files
+        // Currently, this is emulated using localStorage the same way
+        // localFolder is, so naming conflicts could easily arise/
+        FileIO: {
+            writeTextAsync(filename, string) {
+                // TODO: error check for if file does not exist
+
+                localStorage.setItem(filename, string);
+
+                return {
+                    done(success_func, error_func) {
+                        // TODO: figure out when error_func would run
+                        success_func();
+                    }
+                };
+            },
+
+            readTextAsync(filename) {
+                let localStorageData = localStorage.getItem(filename);
+
+                return {
+                    done(success_func, error_func) {
+                        if (localStorageData === null) {
+                            error_func("The file " + filename + " doesn't exists.")
+                        }
+                        else {
+                            success_func(localStorageData);
+                        }
+                    }
+                };
+            }
+
+        },
+
+        // Enum for managing file collisions
+        CreationCollisionOption: {
+            // TODO: replace this with an actual enum
+            GenerateUniqueName: "GenerateUniqueName",
+            replaceExisting: "replaceExisting",
+            FailIfExists: "FailIfExists",
+            OpenIfExists: "OpenIfExists"
+        },
+
+        FileAccessMode: {
+            read: 0,
+            readWrite: 1
+        },
+
+        Streams: {
+            RandomAccessStream: {
+                // Note: this is probably completely wrong
+                copyAsync(data, stream) {
+                    stream.stream_data += data;
+                    return {
+                        then(func) {
+                            func()
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    // UI management
+    UI: {
+        ViewManagement: {
+            // Enums for the different ways an app can be viewed
+            ApplicationViewState: {
+                snapped: 2,
+                filled: 1,
+                fullScreenLandscape: 0,
+                fullScreenPortrait: 3
+            },
+
+            ApplicationView: {
+                value: 0
+            }
+        },
+
+        WebUI: {
+            WebUIApplication: {
+                addEventListener(listenerType, func, useCapture) {
+                    switch (listenerType) {
+                        case "suspending":
+                            // there isn't really a browser equivalent to suspension
+                            break;
+                        case "resuming":
+                            // there isn't really a browser equivalent to resuming
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        },
+
+        ApplicationSettings: {
+            SettingsPane: {
+                // Returns a SettingsPane object
+                getForCurrentView() {
+                    return {
+                        addEventListener(eventName, func, options) {
+                            // TODO: provide emulation for opening settings and stuff
+                        }
+                    }
+                }
+            },
+
+            SettingsCommand: class {
+                constructor(settingsCommandId, label, handler) {
+                    this.Id = settingsCommandId;
+                    this.Label = label;
+                    this.Invoked = handler;
+                }
+                
+                static get accountsCommand() {
+                    return new this(
+                        "accounts",
+                        "Accounts",
+                        null
+                    );
+                }
+            }
+        }
+    },
+
+    // Graphics
+    Graphics: {
+        Display: {
+            // Properties of the current display
+            DisplayProperties: {
+                currentOrientation: 1 // Landscape
+            }
+        }
+    },
+
+    System: {
+        UserProfile: {
+            UserInformation: {
+                // Not actually async
+                getDisplayNameAsync() {
+                    return {
+                        done(func) {
+                            func("Jeffery Epstein")
+                        }
+                    }
+                }
+            },
+
+            GlobalizationPreferences: {
+                homeGeographicRegion: "US",
+                languages: ["en-US"]
+            }
+        }
+    },
+
+    // Apparently there's a whole section in the API for 
+    // connected sensors and devices.
+    Devices: {
+        Sensors: {
+            // Note: since we're only doing PC emulation, Mobile sensors will not be emulated
+            Accelerometer: {
+                getDefault() {
+                    return null;
+                }
+            },
+            
+            Inclinometer: {
+                getDefault() {
+                    return null;
+                }
+            }
+        }
+    },
+
+    Security: {
+        Authentication: {
+            Web: {
+                WebAuthenticationStatus: {
+                    success: 0,
+                    userCancel: 1,
+                    errorHttp: 2
+                },
+
+                WebAuthenticationOptions: {
+                    default: 0, // this may just be a typo
+                    none: 0,
+                    silentMode: 1,
+                    useTitle: 2,
+                    useHttpPost: 4,
+                    useCorporateNetwork: 8
+                },
+
+                WebAuthenticationBroker: {
+                    // TODO: make this async and try to do actual web authentication
+                    // Currently, this just returns an error
+                    authenticateAsync(options, startURI, endURI) {
+                        return {
+                            done(func) {
+                                func(2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+(function () {
+
+    //TODO: extract the appID from AppxManifest.xml
+    Windows.ApplicationModel.Store.CurrentApp.appId = {
+        toString() {
+            return "BF13D240-22AF-45CF-9344-F41E914CEB1A";
+        }
+    } 
+
+    Windows.ApplicationModel.Store.CurrentAppSimulator = Windows.ApplicationModel.Store.CurrentApp
+
+})();
+
+
+
+window.MSApp = {
+    execUnsafeLocalFunction(func) {
+        return func();
+    }
+}
+
+// WinJS.Utilities.startLog();
